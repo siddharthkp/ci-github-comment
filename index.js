1c493a5b629e662258973e65a0f79029e59db4f9
Siddharth Kshetrapal <siddharth.kshetrapal@gmail.com>
2017-11-09 17:57:00 +0530

    let the debugging begin

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const githubComment = require('github-comment')
 const { repo, pull_request_number } = require('ci-env')
 const token = process.env.github_token || process.env.GITHUB_TOKEN
 
+console.log(!!token, repo, pull_request_number, body)
+
 const comment = body => githubComment(token, repo, pull_request_number, body)
 
 module.exports = comment
